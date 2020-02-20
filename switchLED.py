c00904d5584f6bde9c903e6014cb4f96f7d6f406
nthswm <natthawut_mix@hotmail.com>
2020-02-19 23:14:25 -1200

    add Acid PullWater EC(AB)

--- switchLED.py
+++ switchLED.py
@@ -8,11 +8,20 @@ import paho.mqtt.client as mqtt
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(14,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(2,GPIO.IN, pull_up_down=GPIO.PUD_UP) #Acide input
 GPIO.setup(15,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT) #Pull Water Output
+GPIO.setup(4,GPIO.OUT) #EC Output
 
+AcidMoniter_status = "OFF"
 led_status = "OFF"
 sw_status = "0"
 mqtt_status = "0"
+Acid_status = "0"
+pw_status = "0"
+pwMoniter_status = "OFF"
+ec_status = "0"
+ecMoniter_status = "OFF"
 
 def on_message(client, userdata, message):
 	#set mqtt_status
@@ -51,6 +60,15 @@ while True:
     	elif state == True:		
 		sw_status =  "0"
 
+        #Read Acid
+        state_Acid = GPIO.input(2)
+	if state_Acid == False:
+		client.publish("AcidMoniter_status", "ON")
+		
+    	elif state_Acid == True:		
+		client.publish("AcidMoniter_status", "OFF")
+		
+        
 	#LED control
 	if sw_status == "1" or mqtt_status == "1":
         	GPIO.output(15,GPIO.HIGH) 
@@ -65,5 +83,35 @@ while True:
 			led_status = "OFF"
 			#publish
 			client.publish("led_status", "OFF")
+			print("OFF")
+        #PullWater
+        if pw_status == "1" or mqtt_status == "1":
+        	GPIO.output(3,GPIO.HIGH) 
+		if pwMoniter_status == "OFF":
+			pwMoniter_status = "ON"
+			#publish
+			client.publish("pwMoniter_status", "ON")
+			print("ON")
+        elif pw_status == "0" or mqtt_status == "0":
+    	    	GPIO.output(3,GPIO.LOW)
+		if pwMoniter_status =="ON":
+			pwMoniter_status = "OFF"
+			#publish
+			client.publish("pwMoniter_status", "OFF")
+			print("OFF")
+        #EC
+        if ec_status == "1" or mqtt_status == "1":
+        	GPIO.output(4,GPIO.HIGH) 
+		if ecMoniter_status == "OFF":
+			ecMoniter_status = "ON"
+			#publish
+			client.publish("ecMoniter_status", "ON")
+			print("ON")
+        elif ec_status == "0" or mqtt_status == "0":
+    	    	GPIO.output(4,GPIO.LOW)
+		if ecMoniter_status =="ON":
+			ecMoniter_status = "OFF"
+			#publish
+			client.publish("ecMoniter_status", "OFF")
 			print("OFF")
 	
