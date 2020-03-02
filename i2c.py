2c0dee911bec1e01243fbe4caf5905af7a6c63be
pathomphong-p <pathomphong.i@ku.th>
2020-03-02 21:16:49 +0700

    at switch_status

--- i2c.py
+++ i2c.py
@@ -6,8 +6,9 @@ import Adafruit_ADS1x15
 
 adc=Adafruit_ADS1x15.ADS1115()
 GAIN =1
-while 1:
+adc_chanal = 0
+while True:
       
-      adc_values = adc.read_adc(0,gain=GAIN)
+      adc_values = adc.read_adc(adc_chanal,gain=GAIN)
       volt = adc_values * 0.000125
       print("volt =",volt)
