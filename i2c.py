734cd8faba1443c8e4538e02506929d37129dd72
pathomphong-p <pathomphong.i@ku.th>
2020-04-04 20:28:50 +0700

    edit i2c eg

--- i2c.py
+++ i2c.py
@@ -1,13 +1,9 @@
-import RPi.GPIO as GPIO
-import socket
 import time
-import paho.mwqtt.client as mqtt
 import Adafruit_ADS1x15
+adc = Adafruit_ADS1x15.ADS1115()
 
-adc=Adafruit_ADS1x15.ADS1115()
 GAIN =1
-while 1:
-      
-      adc_values = adc_read(0,gain=GAIN)
+while 1:  
+      adc_values = adc.read_adc(0,gain=GAIN)
       volt = adc_values * 0.000125
-      print("volt ="volt)
+      print("volt =",volt)
