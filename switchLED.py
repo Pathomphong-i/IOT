5f58e467ae945ae97d9a0789163db46d4e5529cb
Pathomphong <pathomphong.i@ku.th>
2020-04-17 13:52:01 +0700

    change waterloop logic

--- switchLED.py
+++ switchLED.py
@@ -121,13 +121,13 @@ while True:
 		pi_switch_status =  "OFF" 
 
 	if pi_switch_status == "ON" or mqtt_switch_status == "ON":
-		GPIO.output(led_pin,GPIO.HIGH) 
+		GPIO.output(led_pin,GPIO.LOW) 
 		if pi_led_status == "OFF":
 			pi_led_status = "ON"
 			client.publish("pi_led_status", "ON")
 			print("Led_ON")
 	elif pi_switch_status == "OFF" or mqtt_switch_status == "OFF":
-		GPIO.output(led_pin,GPIO.LOW)
+		GPIO.output(led_pin,GPIO.HIGH)
 		if pi_led_status =="ON":
 			pi_led_status = "OFF"
 			client.publish("pi_led_status", "OFF")
